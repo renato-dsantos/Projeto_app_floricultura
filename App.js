4d901401223e8307a20abff65674f9026e8abf9c
RENATO DOS SANTOS <renato.santos25@senacsp.edu.br>
2026-09-02 21:26:23 -0300

    feat: criando a tela de login com válidação de campo em branco

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, ImageBackground, TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/img.jpg')} style={styles.backgroundImage} imageStyle={{opacity: 0.3}}>
+      <Text>Login</Text>
+        <TextInput
+            style={styles.input}
+            placeholder="Digite o seu login..."
+        />
+        <Text>Senha</Text>
+        <TextInput
+            style={styles.input}
+        />
+        <View>
+          <Button title="Acessar" color="green"/>
+          <Button title="Cancelar" color="blue" />
+        </View>
+
+      </ImageBackground>
+      
     </View>
   );
 }
@@ -16,5 +31,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: '100%',
+    padding: 10,
+    borderWidth: 5,
+    borderColor: '#000',
+    marginBottom: 20,
+  }, 
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
   },
 });
